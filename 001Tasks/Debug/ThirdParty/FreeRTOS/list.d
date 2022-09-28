bc58f05fe78e76c42a452831dfbe8d235ee92936
xuanjiao <xuanjiao.zhu@gmail.com>
2022-09-28 11:58:59 +0200

    first commit

---
+++ 001Tasks/Debug/ThirdParty/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+ThirdParty/FreeRTOS/list.o: ../ThirdParty/FreeRTOS/list.c \
+ D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h \
+ D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h \
+ D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h
+D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/projdefs.h:
+D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/portable.h:
+D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+D:/Workspace/RTOS_workspace/001Tasks/ThirdParty/FreeRTOS/include/list.h:
